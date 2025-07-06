8c56f187365306025efb6cdcc87a2979051dcd8c
YouGo <xoyougoox@gmail.com>
2025-07-07 01:00:17 +0200

    Refact:  (#3)
    * refac: migrate to a single tsconfig file, added esbuild
    
    * refactor: migrate codebase to explicit ES module imports/exports for all files

--- src/content/videospeed/VideoSpeed.ts
+++ src/content/videospeed/VideoSpeed.ts
@@ -1,3 +1,16 @@
+/* 
+ * Copyright (C) 2025-present YouGo (https://github.com/youg-o)
+ * This program is licensed under the GNU Affero General Public License v3.0.
+ * You may redistribute it and/or modify it under the terms of the license.
+ * 
+ * Attribution must be given to the original author.
+ * This program is distributed without any warranty; see the license for details.
+ */
+
+import { ExtensionSettings } from "../../types/types";
+import { videoSpeedLog, videoSpeedErrorLog } from "../../utils/logger";
+
+
 async function syncVideoSpeedPreference() {
     try {
         const result = await browser.storage.local.get('settings');
@@ -15,7 +28,7 @@ async function syncVideoSpeedPreference() {
 }
 
 // Call this function during initialization
-async function handleVideoSpeed() {   
+export async function handleVideoSpeed() {   
     await syncVideoSpeedPreference(); // Sync speed preference
     
     // Check if we should apply speed to current page
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,8 +1,24 @@
+/* 
+ * Copyright (C) 2025-present YouGo (https://github.com/youg-o)
+ * This program is licensed under the GNU Affero General Public License v3.0.
+ * You may redistribute it and/or modify it under the terms of the license.
+ * 
+ * Attribution must be given to the original author.
+ * This program is distributed without any warranty; see the license for details.
+ */
+
+import { coreLog } from "./logger";
+import { currentSettings } from "../content/index"
+import { handleVideoQuality } from "../content/videoquality/VideoQuality";
+import { handleVideoSpeed } from "../content/videospeed/VideoSpeed";
+import { handleSubtitlesPreference } from "../content/subtitles/SubtitlesPreference";
+import { handleAudioNormalizer } from "../content/audionormalizer/AudioNormalizer";
+
 /**
  * Ensures that user settings contain all properties from default settings
  * If a property is missing, it's added with the default value
  */
-function migrateSettings(userSettings: any, defaultSettings: any): any {
+export function migrateSettings(userSettings: any, defaultSettings: any): any {
     // Create a deep copy to avoid modifying the original
     const settings = JSON.parse(JSON.stringify(userSettings));
     let hasChanges = false;
@@ -36,7 +52,7 @@ function migrateSettings(userSettings: any, defaultSettings: any): any {
     return settings;
 }
 
-function applyVideoPlayerSettings(): void {
+export function applyVideoPlayerSettings(): void {
     currentSettings?.videoQuality.enabled && handleVideoQuality();
     currentSettings?.videoSpeed.enabled && handleVideoSpeed();
     currentSettings?.subtitlesPreference.enabled && handleSubtitlesPreference();
